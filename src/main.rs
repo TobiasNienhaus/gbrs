9a8b10a63c1f134e32357a666141496a8ec60223
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-04-28 19:55:55 +0200

    Add a simple framebuffer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use minifb::{Key, Window, WindowOptions, KeyRepeat};
+
+// Actually 16742.005692282 microseconds
+const REFRESH_RATE: u64 = 16742;
+
+const SCREEN_WIDTH: usize = 160;
+const SCREEN_HEIGHT: usize = 144;
+const PIXEL_COUNT: usize = SCREEN_WIDTH * SCREEN_HEIGHT;
+
+type FrameBuffer = [u32; PIXEL_COUNT];
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer: FrameBuffer = [0; PIXEL_COUNT];
+    for i in buffer.iter_mut() {
+        *i = rand::random();
+    }
+
+    let mut window = Window::new(
+        "GBRS",
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        WindowOptions::default()
+    ).unwrap();
+
+    window.limit_update_rate(Some(std::time::Duration::from_micros(REFRESH_RATE)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        if window.is_key_pressed(Key::Space, KeyRepeat::No) {
+            for i in buffer.iter_mut() {
+                *i = rand::random();
+            }
+        }
+
+        window
+            .update_with_buffer(&buffer, SCREEN_WIDTH, SCREEN_HEIGHT)
+            .unwrap();
+    }
 }
